7cbf227fed2287da7c8f211d1f6fe0a515d192c1
Ricky Giese <richard.giese@icloud.com>
2023-07-29 12:31:42 -0500

    Quiz container CSS

---
+++ script.js
@@ -0,0 +1,73 @@
+const quizData = [
+    {
+        question: 'Who was Anikin Skywalker\'s padawan?',
+        a: 'Obi-Wan Kenobi',
+        b: 'Qui-Gon Jinn',
+        c: 'Ahsoka Tano',
+        d: 'Darth Maul', 
+        correct: 'c'
+    }, {
+        question: 'What is of Count Dooku\'s first Sith apprentice?',
+        a: 'Darth Maul',
+        b: 'Asajj Ventress',
+        c: 'General Grevious',
+        d: 'Anakin Skywalker',
+        correct: 'b'
+    }, {
+        question: 'What is the name of Darth Maul\'s brother, who becomes his apprentice?',
+        a: 'Embo',
+        b: 'Jango Fett',
+        c: 'Asajj Ventress',
+        d: 'Savage Opress',
+        correct: 'd'
+    }, {
+        question: 'What year did TV Series Star Wars: The Clone Wars first air',
+        a: '1977',
+        b: '1999',
+        c: '2008',
+        d: '2015',
+        correct: 'c'
+    }, {
+        question: 'Who was Count Dooku\'s Jedi padawan?',
+        a: 'Qui-Gon Jinn',
+        b: 'Obi-Wan Kenobi',
+        c: 'Plo Koon',
+        d: 'Mace Windu',
+        correct: 'a'
+    }, {
+        question: 'Which Droid did Anakin Skywalker build?',
+        a: 'R2-D2',
+        b: 'C-3PO',
+        c: '4-LOM',
+        d: 'BD-1',
+        correct: 'b'
+    }, {
+        question: 'Padme Amidala is the senator of what planet?',
+        a: 'Tatooine',
+        b: 'Coruscant',
+        c: 'Bespin',
+        d: 'Naboo',
+        correct: 'd'
+    }, {
+        questions: 'What Planet was home to the Jedi Council?',
+        a: 'Bespin',
+        b: 'Alderaan',
+        c: 'Corellia',
+        d: 'Coruscant',
+        correct: 'c'
+    }, {
+        question: 'Who said \"Had you said the word, I would have left the Jedi order.\"',
+        a: 'Mace Windu',
+        b: 'Anakin Skywalker',
+        c: 'Obi-Wan Kenobi',
+        d: 'Ahsoka Tano',
+        correct: 'c'
+    }, {
+        question: 'What clone protocol is used to label all Jedi as traitors of the republic?',
+        a: 'Order 5',
+        b: 'Order 37',
+        c: 'Order 65',
+        d: 'Order 66',
+        correct: 'd'
+    }, 
+]
